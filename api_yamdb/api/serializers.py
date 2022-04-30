6df88ff529a03ca552e955b71afc3c686390af98
Даниил Бородкин <faaanmu@gmail.com>
2022-04-30 11:28:56 +0300

    Changed auth views

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
 User = get_user_model()
 
@@ -18,10 +17,3 @@ class UserSerializer(serializers.ModelSerializer):
 class TokenSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=150)
     confirmation_code = serializers.CharField(max_length=150)
-
-    def validate(self, data):
-        if not User.objects.filter(username=data['username']).exists():
-            raise ValidationError(
-                'Пользователь с таким username не существует!'
-            )
-        return data
